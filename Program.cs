3f45a5f30254b3101ed7b3f4d7a760306e14002f
danielvanderuit <dm.vanderuit@student.han.nl>
2021-04-15 15:50:28 +0200

    Add generated code

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AzureADDemo
+namespace WebApp_OpenIDConnect_DotNet
 {
     public class Program
     {
@@ -18,6 +18,9 @@ namespace AzureADDemo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
